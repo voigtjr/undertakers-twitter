defddc4d8b5a43d00c6f987e9976271f19a52561
Jonathan Voigt <voigtjr@gmail.com>
2011-09-22 21:46:40 -0400

    Added getters for keys via environment variables

--- src/undertakers_twitter/core.clj
+++ src/undertakers_twitter/core.clj
@@ -1,1 +1,26 @@
 (ns undertakers-twitter.core)
+
+(require 'twitter
+  ['oauth.client :as 'oauth])
+
+;; Make a OAuth consumer
+(def oauth-consumer (oauth/make-consumer (System/getenv "UNDERTAKERS_KEY")
+  (System/getenv "UNDERTAKERS_SECRET")
+  "https://api.twitter.com/oauth/request_token"
+  "https://api.twitter.com/oauth/access_token"
+  "https://api.twitter.com/oauth/authorize"
+  :hmac-sha1))
+
+;; token/secret
+(def oauth-access-token (System/getenv "UNDERTAKERSNOVI_TOKEN"))
+
+(def oauth-access-token-secret (System/getenv "UNDERTAKERSNOVI_SECRET"))
+
+;; Post to twitter
+;; (twitter/with-oauth oauth-consumer
+;;   oauth-access-token
+;;   oauth-access-token-secret
+;;   (twitter/update-status "posting from #clojure with #oauth"))
+
+;; Find out who follows dons
+(twitter/followers-of-name "undertakersnovi")
