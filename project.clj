dd764e56440acaa691d9d05b357dff1fd641bafd
Jonathan Voigt <voigtjr@gmail.com>
2011-09-22 20:52:35 -0400

    Updated description

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
 (defproject undertakers-twitter "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Posts reminders and what-not to the Undertakers Twitter feed" 
   :dependencies [[org.clojure/clojure "1.2.1"]])
