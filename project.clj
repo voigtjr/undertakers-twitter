0215a7a0ff6665a30a06ac43886907a05998def4
Jonathan Voigt <voigtjr@gmail.com>
2011-09-22 20:55:55 -0400

    Changed the license to the EPL because that's easier

--- project.clj
+++ project.clj
@@ -1,3 +1,7 @@
 (defproject undertakers-twitter "1.0.0-SNAPSHOT"
   :description "Posts reminders and what-not to the Undertakers Twitter feed" 
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"
+            :distribution :repo
+            :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.2.1"]])
