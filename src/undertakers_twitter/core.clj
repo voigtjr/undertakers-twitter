43e9bb91095f5585d1917b72afcd9dc01a3a6017
Jonathan Voigt <voigtjr@gmail.com>
2011-09-22 20:51:30 -0400

    Initial import

---
+++ src/undertakers_twitter/core.clj
@@ -0,0 +1,1 @@
+(ns undertakers-twitter.core)
---
+++ test/undertakers_twitter/test/core.clj
@@ -0,0 +1,6 @@
+(ns undertakers-twitter.test.core
+  (:use [undertakers-twitter.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
