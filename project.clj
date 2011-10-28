04bdb03d78e108db629ba86cb8a7fe6b6a86ac93
Jonathan Voigt <voigtjr@gmail.com>
2011-10-27 23:43:19 -0400

    Added some new deps, autotesting script

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
+;; TODO Move to Clojure 1.3
 (defproject undertakers-twitter "1.0.0-SNAPSHOT"
   :description "Posts reminders and what-not to the Undertakers Twitter feed" 
-  :main "undertakers-twitter.core"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.2.1"]
-                 [clojure-twitter "1.2.5"]])
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [clojure-twitter "1.2.5"]
+                 [joda-time "2.0"]]
+  :dev-dependencies [[com.stuartsierra/lazytest "1.1.2"]]
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
+  :main "undertakers-twitter.core")
